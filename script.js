fd3b64691e5818d8ddacd59cc519fe12d1703fd3
agata0407 <152336826+agata0407@users.noreply.github.com>
2023-11-28 22:47:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function sayHello() {
+    const name = prompt("What is your name?");
+    alert("Hello, " + name + "! Welcome to my website.");
+}
+
+sayHello();
+
+function rollDice() {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    alert("You have rolled a " + dice + "!");
+}
+
+rollDice();
+
+function jobSearch() {
+    let months = 0;
+    let dice = Math.floor(Math.random() * 6) + 1;
+    months += dice;
+    alert("It will take you " + months + " months to get your first job.");
+}
+
+jobSearch();
+
+function comfortLevel (html, css, js) {
+    const average = (html + css + js) / 3;
+    if (average >= 7) {
+        console.log("You are a pro!");
+    } else if (average >=4) {
+        console.log("Don't worry you are nearly there");
+    } else {
+        console.log("If you keep trying you will get there!");
+    }
+
+}
+
+comfortLevel(15,20,3);
+
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.body.style.backgroundImage = "none";
+    document.getElementById("#"). style.color = "white";
+}
